149fc7bac5f0dc4215dc3226812acd671628aa36
Aaron Makaruk <aaronmakaruk@gmail.com>
2026-01-09 14:54:57 -0700

    Lesson 5: Map

---
+++ AppleLandmark/MapView.swift
@@ -0,0 +1,31 @@
+//
+//  MapView.swift
+//  AppleLandmark
+//
+//  Created by Aaron Makaruk on 1/9/26.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion {
+
+            MKCoordinateRegion(
+
+                center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+
+                span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+
+            )
+
+        }
+}
+
+#Preview {
+    MapView()
+}
