406e40fc68fd836162a15c4e4e50856ada25addd
Aaron Makaruk <aaronmakaruk@gmail.com>
2026-01-09 14:08:05 -0700

    Initial commit

---
+++ AppleLandmark/AppleLandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLandmarkApp.swift
+//  AppleLandmark
+//
+//  Created by Aaron Makaruk on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct AppleLandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleLandmark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleLandmark
+//
+//  Created by Aaron Makaruk on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
