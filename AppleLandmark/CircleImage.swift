62b1ae95fe4df645e4a7996ca50859a0d3aaadfd
Aaron Makaruk <aaronmakaruk@gmail.com>
2026-01-09 14:28:56 -0700

    Lesson 4

---
+++ AppleLandmark/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  AppleLandmark
+//
+//  Created by Aaron Makaruk on 1/9/26.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
