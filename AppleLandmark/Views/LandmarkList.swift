ff0c8b8f6e444f536ccc0bcbc15a536d4a9480e0
Aaron Makaruk <aaronmakaruk@gmail.com>
2026-01-10 13:29:43 -0700

    Favorites State

--- AppleLandmark/Views/LandmarkList.swift
+++ AppleLandmark/Views/LandmarkList.swift
@@ -1,22 +1,29 @@
-//
-//  LandmarkList.swift
-//  AppleLandmark
-//
-//  Created by Aaron Makaruk on 1/10/26.
-//
-
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter {
+            landmark in (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationSplitView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
         } detail: {
             Text("Select a landmark.")
