33f11adcdd2e518aace4c6e9a72d6eab683e3b23
Aaron Makaruk <aaronmakaruk@gmail.com>
2026-01-12 11:02:12 -0700

    Composing Complex Interfaces

---
+++ AppleLandmark/Views/Categories/CategoryItem.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .foregroundStyle(.primary)
+                .font(.caption)
+        }
+    }
+}
+
+#Preview {
+    CategoryItem(landmark: ModelData().landmarks[0])
+}
